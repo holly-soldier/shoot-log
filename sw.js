554b2ce789e4390d444469bc618fcd65f65b2ec8
holly-soldier <kiseki01@gmail.com>
2026-06-07 10:16:32 +0900

    sw.js を作成

---
+++ sw.js
@@ -0,0 +1,22 @@
+const CACHE_NAME = 'shooting-log-v1';
+const urlsToCache = [
+  './index.html',
+  './manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        // キャッシュがあればそれを返し、なければネットワークから取得
+        return response || fetch(event.request);
+      })
+  );
+});
