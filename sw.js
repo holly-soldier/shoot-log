3ac72b88548d7e80fd142af66dea245e7205baea
holly-soldier <kiseki01@gmail.com>
2026-06-12 07:46:48 +0900

    sw.js を更新

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // キャッシュのバージョンをコードの更新に合わせて変更する
-const CACHE_NAME = 'shooting-log-v26.06.10_6';
+const CACHE_NAME = 'shooting-log-v26.06.10_7';
 const urlsToCache = [
   './index.html',
   './manifest.json'
